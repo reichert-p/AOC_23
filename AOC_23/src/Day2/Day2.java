171130edeab60cea618beedfb344447edb6aeb96
reichert-p <73558016+reichert-p@users.noreply.github.com>
2023-12-02 11:41:17 +0100

    day2

--- AOC_23/src/Day2/Day2.java
+++ AOC_23/src/Day2/Day2.java
@@ -5,8 +5,38 @@ import Helper.Input;
 public class Day2 {
     public static void main(String[] args) {
         String[] lines = Input.readStringLines("Day2/input.txt");
-        for (String line: lines) {
+        int sum = 0;
+        for (int id = 1; id <= lines.length; id ++) {
+            String game = lines[id -1];
+            var removeFirst = game.split(":")[1];
+            var sets = removeFirst.split(";");
+            //boolean possible = true; // uncomment these lines for part one
+            int maxRed = 0;
+            int maxGreen = 0;
+            int maxBlue = 0;
+            for (String set: sets) {
+                for (String colorCombinaton: set.split(",")) {
+                    colorCombinaton = colorCombinaton.trim();
+                    String numberString = colorCombinaton.split(" ")[0];
+                    int number = Integer.valueOf(numberString);
 
+                    if (colorCombinaton.contains("red")){
+                       // possible = possible && number <= 12;
+                        maxRed = Math.max(number, maxRed);
+                    }
+                    else if (colorCombinaton.contains("green")){
+                        //possible = possible && number <= 13;
+                        maxGreen = Math.max(number, maxGreen);
+                    }
+                    else if (colorCombinaton.contains("blue")){
+                       // possible = possible && number <= 14;
+                        maxBlue = Math.max(number, maxBlue);
+                    }
+                }
+            }
+            System.out.println(maxRed * maxGreen * maxBlue);
+            sum += maxRed * maxGreen * maxBlue;
         }
+        System.out.println(sum);
     }
 }
---
+++ AOC_23/src/Day3/Day3.java
@@ -0,0 +1,9 @@
+package Day3;
+
+import Helper.Input;
+
+public class Day3 {
+    public static void main(String[] args) {
+        String[] lines = Input.readStringLines("Day3/input.txt");
+    }
+}
